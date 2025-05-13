24f94c0d3cac995d09e348b6d5093a76b4b1a882
reddybro108 <mail2amolreddy@gmail.com>
2025-05-13 12:51:30 +0530

    Update main app, requirements, and add test case

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = FastAPI()
 class Greetrequest(BaseModel):
     name: str
 
-
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI in Docker!"}
@@ -14,5 +13,44 @@ def read_root():
 @app.post("/greet/{name}")
 def greet_user(name: str):
     return {"message": f"Hello, {name}!"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# class Greetrequest(BaseModel):
+#     name: str
+
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Welcome to the FastAPI example!"}
+
+# @app.post("/greet/{name}")
+# def greet_user(name: str):
+#     return {"message": f"Hello, {name}!"}
 
 
--- test_main.py
+++ test_main.py
@@ -8,7 +8,27 @@ def test_get_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from FastAPI in Docker!"}
+
 def test_greet_user():
     response = client.post("/greet/John")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, John!"}    
+    assert response.json() == {"message": "Hello, John!"}
+
+
+
+
+
+# from fastapi.testclient import TestClient
+# from main import app
+# import httpx
+
+# client = TestClient(app)
+
+# def test_get_root():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Welcome to the FastAPI example!"}
+# def test_greet_user():
+#     response = client.post("/greet/John")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Hello, John!"}    
